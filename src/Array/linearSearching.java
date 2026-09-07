103c7077b65d70ad53252611fc041c73e76cea6b
Bijoy Das <bijoydas6291.com@gmail.com>
2026-09-08 02:55:00 +0530

    updated

--- src/Array/linearSearching.java
+++ src/Array/linearSearching.java
@@ -1,7 +1,7 @@
-//Take an array as input from the user. Search for a given number x and print the index at which it occurs.
+package Array;//Take an array as input from the user. Search for a given number x and print the index at which it occurs.
 
 import java.util.*;
-public class ArrPrac {
+public class linearSearching {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int input = sc.nextInt();
@@ -11,7 +11,6 @@ public class ArrPrac {
         }
         System.out.print("enter the number: ");
         int x = sc.nextInt();
-
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == x) {
                 System.out.println("x found at index: " + i);
