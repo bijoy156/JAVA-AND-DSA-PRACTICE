5ec3d3fb1b24a4c4f8cbdb1a0d0ce593f1f7cda0
Bijoy Das <bijoydas6291.com@gmail.com>
2026-09-11 18:01:36 +0530

    updated

---
+++ src/Array/sorted.java
@@ -0,0 +1,38 @@
+package Array;
+import java.util.*;
+public class sorted {
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        boolean dec = isDecreasing(arr);
+        boolean inc = isIncreasing(arr);
+        if(dec||inc){
+            System.out.println("Your array is sorted");
+        }
+        else{
+            System.out.println("NOT SORTED");
+        }
+    }
+    public static boolean isIncreasing(int[]arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i - 1] > arr[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static boolean isDecreasing(int[]arr){
+        for(int i=1; i<arr.length; i++){
+            if(arr[i-1] < arr[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
