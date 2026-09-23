5e1266427ff0aa361ccb4d72cab575c12d25bb37
Bijoy Das <bijoydas6291.com@gmail.com>
2026-09-24 02:52:10 +0530

    updated

--- src/Array/SecMax.java
+++ src/Array/SecMax.java
@@ -27,3 +27,29 @@ public class SecMax {
         return SecMax;
     }
 }
+
+//APPROACH 2
+//class Solution {
+//    public int secondLargestElement(int[] arr) {
+//        int largest = arr[0];
+//        for(int i=0; i<arr.length; i++){
+//            if(largest<arr[i]){
+//                largest = arr[i];
+//            }
+//        }
+//        int secLargest = Integer.MIN_VALUE;
+//        for(int i=1; i<arr.length; i++){
+//            if(secLargest<arr[i] && arr[i]<largest){
+//                secLargest=arr[i];
+//            }
+//
+//        }
+//        if(secLargest == Integer.MIN_VALUE){
+//            return -1;
+//        }
+//        return secLargest;
+//
+//    }
+//
+//}
+
